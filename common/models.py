a97d293182725990c9e51dfd0a4361a0b3255a6e
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-13 18:16:33 +0330

    feat: common models - BaseModel ID field
    Overrode the ID field by generating an ID using UUID

--- common/models.py
+++ common/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 
@@ -6,6 +8,7 @@ class BaseModel(models.Model):
     Base model for all models which includes minimum required date fields
     """
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
