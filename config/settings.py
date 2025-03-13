2f5981613c958303c64b64e876eca82f6fbd9059
Amirhosein Malekpour <work.malekpour99@gmail.com>
2025-03-13 20:17:35 +0330

    chore: config settings - rest framework registeration
    Registered Django REST framework in the config settings

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     # Project Apps
     "common.apps.CommonConfig",
     "wallet.apps.WalletConfig",
